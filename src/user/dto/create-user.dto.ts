7db96bf79de3475fcb171e1209a53c93d21c0633
Ev-geek-niy <zhenya.vilgauk@gmail.com>
2021-12-21 18:13:39 +0500

    Добавлен CRUD для записей, комментариев, связал комментарий с ID поста и ID пользователя

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @Length(3)
   fullname: string;
+
+  @IsEmail({ allow_display_name: true }, { message: 'Неверная почта' })
   email: string;
+
+  @Length(6, 32, { message: 'Пароль должен быть не менее 6 символов' })
   password?: string;
 }

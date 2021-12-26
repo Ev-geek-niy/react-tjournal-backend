566c9a54448bad402a8222a5864080644b3cf766
Ev-geek-niy <zhenya.vilgauk@gmail.com>
2021-12-26 12:04:43 +0500

    Добавлена авторизация PassportJS

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,11 +2,11 @@ import { IsEmail, Length } from 'class-validator';
 
 export class CreateUserDto {
   @Length(3)
-  fullname: string;
+  fullName: string;
 
   @IsEmail({ allow_display_name: true }, { message: 'Неверная почта' })
   email: string;
 
-  @Length(6, 32, { message: 'Пароль должен быть не менее 6 символов' })
+  @Length(3, 32, { message: 'Пароль должен быть не менее 6 символов' })
   password?: string;
 }

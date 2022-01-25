7ecd3e491fa227f4e037de2384b6aea3fbca163c
Ev-geek-niy <zhenya.vilgauk@gmail.com>
2022-01-25 15:45:41 +0500

    Добавлена регистрация пользователя, редактирование профиля, поиск по всем пользователям

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,10 +1,15 @@
 import { IsEmail, Length } from 'class-validator';
+import { UniqueOnDatabase } from '../../auth/validations/UniqueValidation';
+import { UserEntity } from '../entities/user.entity';
 
 export class CreateUserDto {
   @Length(3)
   fullName: string;
 
   @IsEmail({ allow_display_name: true }, { message: 'Неверная почта' })
+  @UniqueOnDatabase(UserEntity, {
+    message: 'Такая почта уже есть',
+  })
   email: string;
 
   @Length(3, 32, { message: 'Пароль должен быть не менее 6 символов' })

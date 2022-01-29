4126606bc263b009d6f593296adad11cd8fc1aab
Ev-geek-niy <zhenya.vilgauk@gmail.com>
2022-01-29 17:47:58 +0500

    Доработаны методы, исправлены ошибки

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -12,16 +12,33 @@ export class CommentService {
     private repository: Repository<CommentEntity>,
   ) {}
 
-  create(dto: CreateCommentDto) {
-    return this.repository.save({
+  async create(dto: CreateCommentDto, userId: number) {
+    const comment = await this.repository.save({
       text: dto.text,
       post: { id: dto.postId },
-      user: { id: 1 },
+      user: { id: userId },
     });
+    return this.repository.findOne({ id: comment.id }, { relations: ['user'] });
   }
 
-  findAll() {
-    return this.repository.find();
+  async findAll(postId: number) {
+    const qb = await this.repository.createQueryBuilder('c');
+
+    if (postId) {
+      qb.where('c.postId = :postId', { postId });
+    }
+
+    const result = await qb
+      .leftJoinAndSelect('c.post', 'post')
+      .leftJoinAndSelect('c.user', 'user')
+      .getMany();
+
+    return result.map((obj) => {
+      return {
+        ...obj,
+        post: { id: obj.post.id, title: obj.post.title },
+      };
+    });
   }
 
   findOne(id: number) {
